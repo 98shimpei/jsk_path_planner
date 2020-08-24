b25b22d04757c1ad3f68d5588c64b1852a23df2c
Yuki Omori <ohmori@jsk.imi.i.u-tokyo.ac.jp>
2020-08-24 17:38:25 +0900

    add self filter

---
+++ scripts/robot_bbox_sf_publisher.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import rospy
+from jsk_recognition_msgs.msg import BoundingBox
+from geometry_msgs.msg import Point, Quaternion, Vector3
+
+rospy.init_node("robot_bbox_sf_publisher", anonymous=True)
+p = rospy.Publisher("/robot_bbox_sf", BoundingBox, queue_size=10)
+r = rospy.Rate(100)
+
+bbox_msg = BoundingBox()
+bbox_msg.header.frame_id = 'body_on_odom'
+
+position = Point(0.0, 0.0, 1.0)
+orientation = Quaternion(0.0, 0.0, 0.0, 0.0)
+dimensions = Vector3(0.8, 1.6, 1.6)
+
+bbox_msg.pose.position = position
+bbox_msg.pose.orientation = orientation
+bbox_msg.dimensions = dimensions
+
+while not rospy.is_shutdown():
+  p.publish(bbox_msg)
+  r.sleep()
